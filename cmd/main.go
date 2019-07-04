f55332335e0a4ffa2d588b92ad62554f63b12e71
Robin Hung <robin840615@gmail.com>
2019-07-04 14:58:38 -0400

    get esm patch titles and dates

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ import (
 
 type ubuntuEsmPatches struct {
 	Title string
+	Date  string
 	CVEs  []string
 }
 
@@ -27,33 +28,45 @@ func main() {
 		// colly.MaxDepth(3),
 	)
 
-	var cves []string
+	// var cves []string
+	patches := []ubuntuEsmPatches{}
+	var dates []string
+	var titles []string
+
+	c.OnHTML(".p-heading--four", func(e *colly.HTMLElement) {
+		titles = append(titles, e.Text)
+
+		e.ForEach("a[href]", func(_ int, e *colly.HTMLElement) {
+			link := e.Attr("href")
+			if !strings.HasPrefix(link, "https://usn.ubuntu.com/") {
+				return
+			}
+			c.Visit(e.Request.AbsoluteURL(link))
+		})
+	})
+
+	c.OnHTML("em", func(e *colly.HTMLElement) {
+		dates = append(dates, e.Text)
+	})
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
+	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	// 	link := e.Attr("href")
+	// 	if strings.HasPrefix(e.Text, "CVE-") {
+	// 		// fmt.Println(e.Text, cves)
+	// 		for _, item := range cves {
+	// 			if item == e.Text {
+	// 				return
+	// 			}
+	// 		}
+	// 		cves = append(cves, e.Text)
+	// 	}
 
-		// fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// fmt.Println(link)
-
-		if strings.HasPrefix(e.Text, "CVE-") {
-			// fmt.Println(e.Text, cves)
-			for _, item := range cves {
-				if item == e.Text {
-					return
-				}
-			}
-			cves = append(cves, e.Text)
-		}
-
-		// if !strings.HasPrefix(link, "https://usn.ubuntu.com/") && !strings.HasPrefix(link, "https://people.canonical.com/~ubuntu-security/cve") {
-		// 	return
-		// }
-		if !strings.HasPrefix(link, "https://usn.ubuntu.com/") {
-			return
-		}
-		c.Visit(e.Request.AbsoluteURL(link))
-	})
+	// 	if !strings.HasPrefix(link, "https://usn.ubuntu.com/") {
+	// 		return
+	// 	}
+	// 	c.Visit(e.Request.AbsoluteURL(link))
+	// })
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
@@ -62,6 +75,16 @@ func main() {
 	c.Visit("https://usn.ubuntu.com/releases/ubuntu-14.04-esm/")
 	// c.Visit("https://usn.ubuntu.com/3977-3/")
 
+	for i, t := range titles {
+		patch := ubuntuEsmPatches{}
+		patch.Title = t
+		patch.Date = dates[i]
+		patches = append(patches, patch)
+	}
+
+	// TODO: logging purpose, needs to be removed.
+	fmt.Println(len(titles), len(dates), len(patches))
+
 	enc := json.NewEncoder(os.Stdout)
-	enc.Encode(cves)
+	enc.Encode(patches)
 }
