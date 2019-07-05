5433832b5edf58e4418bc2f78926ed2343bfc579
Robin Hung <robin840615@gmail.com>
2019-07-05 13:01:14 -0400

    cve scores

--- cmd/main.go
+++ cmd/main.go
@@ -19,6 +19,7 @@ type ubuntuEsmPatches struct {
 type cve struct {
 	ID                  string
 	BaseScore           string
+	Severity            string
 	ImpactScore         string
 	ExploitabilityScore string
 }
@@ -29,10 +30,16 @@ func main() {
 		// colly.MaxDepth(3),
 	)
 
+	c2 := colly.NewCollector(
+		colly.AllowedDomains("nvd.nist.gov"),
+	)
+
 	var cves [][]string
 	patches := []ubuntuEsmPatches{}
 	var dates []string
 	var titles []string
+
+	vulns := []cve{}
 
 	c.OnHTML("em", func(e *colly.HTMLElement) {
 		dates = append(dates, e.Text)
@@ -58,6 +65,9 @@ func main() {
 				link := elem.Attr("href")
 				if strings.HasPrefix(link, "https://people.canonical.com/~ubuntu-security/cve/") && strings.HasPrefix(elem.Text, "CVE-") {
 					subString = append(subString, elem.Text)
+
+					cveScoringLink := "https://nvd.nist.gov/vuln/detail/" + elem.Text
+					c2.Visit(cveScoringLink)
 				}
 			})
 		})
@@ -65,7 +75,36 @@ func main() {
 		cves = append(cves, subString)
 	})
 
+	c2.OnHTML("div#p_lt_WebPartZone1_zoneCenter_pageplaceholder_p_lt_WebPartZone1_zoneCenter_VulnerabilityDetail_VulnDetailFormPanel", func(e *colly.HTMLElement) {
+		vuln := cve{}
+		e.ForEach("span", func(_ int, el *colly.HTMLElement) {
+			attr := el.Attr("data-testid")
+			if attr == "page-header-vuln-id" {
+				// fmt.Println(el.Text)
+				vuln.ID = el.Text
+			}
+			if attr == "vuln-cvssv3-base-score" {
+				// fmt.Println(el.Text)
+				vuln.BaseScore = el.Text
+			}
+			if attr == "vuln-cvssv3-base-score-severity" {
+				vuln.Severity = el.Text
+			}
+			if attr == "vuln-cvssv3-impact-score" {
+				vuln.ImpactScore = strings.TrimSpace(el.Text)
+			}
+			if attr == "vuln-cvssv3-exploitability-score" {
+				vuln.ExploitabilityScore = strings.TrimSpace(el.Text)
+			}
+		})
+
+		vulns = append(vulns, vuln)
+	})
+
 	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+	c2.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
@@ -78,6 +117,8 @@ func main() {
 		patch.CVEs = cves[i]
 		patches = append(patches, patch)
 	}
+
+	fmt.Println(vulns, len(vulns))
 
 	// Print out json data
 	// enc := json.NewEncoder(os.Stdout)
